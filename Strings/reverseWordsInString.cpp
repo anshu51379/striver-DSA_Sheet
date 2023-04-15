c40afdf023969498cf61ae26a4342f9a2f7b867c
Abhishek Kumar <80306592+anshu51379@users.noreply.github.com>
2023-04-15 14:32:29 +0530

    Update reverseWordsInString.cpp

--- Strings/reverseWordsInString.cpp
+++ Strings/reverseWordsInString.cpp
@@ -41,3 +41,59 @@ int main()
     }
 }
 // } Driver Code Ends
+
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to reverse words in a given string.
+    string reverseWords(string S) 
+    { 
+        // code here 
+        string tmp="", ans="";
+        int low=0, high=S.size()-1;
+        while(low<=high){
+            if(S[low]=='.'){
+                if(ans!=""){
+                    ans=tmp+'.'+ans;
+                }else{
+                    ans=tmp;
+                }
+                tmp="";
+            }else{
+                tmp+=S[low];
+            }
+            low+=1;
+        }
+        if(tmp!=""){
+            if(ans!=""){
+                ans=tmp+'.'+ans;
+            }else{
+                ans=tmp;
+            }
+        }
+        return ans;
+    } 
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+    int t;
+    cin >> t;
+    while (t--) 
+    {
+        string s;
+        cin >> s;
+        Solution obj;
+        cout<<obj.reverseWords(s)<<endl;
+    }
+}
+// } Driver Code Ends
