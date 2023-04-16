0cda4722ba58b20be5de62203565cc896b9360c3
Abhishek Kumar <80306592+anshu51379@users.noreply.github.com>
2023-04-16 16:25:35 +0530

    Create findLengthOfLinkedList.cpp

---
+++ Linked List/findLengthOfLinkedList.cpp
@@ -0,0 +1,76 @@
+// https://practice.geeksforgeeks.org/problems/count-nodes-of-linked-list/0?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=count-nodes-of-linked-list
+
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+/* Link list node */
+/*
+struct Node
+{
+    int data;
+    Node* next;
+    Node(int x) {  data = x;  next = NULL; }
+}; */
+
+class Solution
+{
+    public:
+    //Function to count nodes of a linked list.
+    int getCount(struct Node* head){
+        //Code here
+        int cnt=1;
+        Node* tmp=head;
+        if(tmp->next==NULL){
+            return 0;
+        }
+        while(tmp->next!=NULL){
+            cnt+=1;
+            tmp=tmp->next;
+        }
+        return cnt;
+    }
+};
+    
+
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+
+        int data;
+        cin>>data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < n-1; ++i)
+        {
+            cin>>data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        Solution ob;
+        cout << ob.getCount(head) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
