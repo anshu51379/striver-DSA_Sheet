b7b66d94597eb28270532cafdd566cd7f1d47a2b
Abhishek Kumar <80306592+anshu51379@users.noreply.github.com>
2023-04-19 01:36:55 +0530

    Update introductionToBitManipulation.cpp

---
+++ Bit Manipulation/introductionToBitManipulation.cpp
@@ -0,0 +1,35 @@
+// https://practice.geeksforgeeks.org/problems/bit-manipulation-1666686020/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=bit-manipulation
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void bitManipulation(int num, int i) {
+        // your code here
+        i--;
+        int get=(num)&(1<<i);
+        int set=(num)|(1<<i);
+        int clear=(num)&(~(1<<i));
+        cout<<(get!=0)<<" "<<set<<" "<<clear;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n >> i;
+        Solution ob;
+        ob.bitManipulation(n, i);
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
