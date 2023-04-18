f50ee062585aac08989e4fea891468077573ee99
Abhishek Kumar <80306592+anshu51379@users.noreply.github.com>
2023-04-19 04:00:02 +0530

    Update divideTwoIntegersWithoutDivision.cpp

--- Bit Manipulation/divideTwoIntegersWithoutDivision.cpp
+++ Bit Manipulation/divideTwoIntegersWithoutDivision.cpp
@@ -55,3 +55,39 @@ int main() {
 }
 
 // } Driver Code Ends
+
+
+
+// https://leetcode.com/problems/divide-two-integers/
+
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if(dividend==INT_MIN and divisor==-1){
+            return INT_MAX;
+        }
+        long long m=labs(dividend), n=labs(divisor), res=0;
+        int sign=((dividend < 0) ^ (divisor < 0))?-1:1;
+        if(n==1){
+            if(sign==1){
+                return m;
+            }else{
+                return -m;
+            }
+        }
+        while(m>=n){
+            long long tmp=n, cnt=1;
+            while(m>=(tmp<<1)){
+                tmp<<=1;
+                cnt<<=1;
+            }
+            res+=cnt;
+            m-=tmp;
+        }
+        if(sign==1){
+            return res;
+        }else{
+            return -res;
+        }
+    }
+};
