1d12d5d58c73ddcfe370b48df25c283744346028
Abhishek Kumar <80306592+anshu51379@users.noreply.github.com>
2023-06-02 14:42:55 +0530

    Update searchInBinarySearchTree.cpp

---
+++ Binary Search Trees/searchInBinarySearchTree.cpp
@@ -0,0 +1,103 @@
+// https://practice.geeksforgeeks.org/problems/search-a-node-in-bst/1
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node *right;
+    Node *left;
+
+    Node(int x) {
+        data = x;
+        right = NULL;
+        left = NULL;
+    }
+};
+
+bool search(Node *root, int x);
+
+Node *insert(Node *tree, int val) {
+    Node *temp = NULL;
+    if (tree == NULL) return new Node(val);
+
+    if (val < tree->data) {
+        tree->left = insert(tree->left, val);
+    } else if (val > tree->data) {
+        tree->right = insert(tree->right, val);
+    }
+
+    return tree;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        Node *root = NULL;
+
+        int N;
+        cin >> N;
+        for (int i = 0; i < N; i++) {
+            int k;
+            cin >> k;
+            root = insert(root, k);
+        }
+
+        int s;
+        cin >> s;
+
+        cout << search(root, s);
+        cout << endl;
+    }
+}
+
+// } Driver Code Ends
+
+
+// Function to search a node in BST.
+bool search(Node* root, int x) {
+    // Your code here
+    if(!root){
+        return false;
+    }
+    if(root->data==x){
+        return true;
+    }
+    if(x<root->data){
+        return search(root->left, x);
+    }else{
+        return search(root->right, x);
+    }
+}
+
+
+
+// https://leetcode.com/problems/search-in-a-binary-search-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        while(root and root->val!=val){
+            if(val<root->val){
+                root=root->left;
+            }else{
+                root=root->right;
+            }
+        }
+        return root;
+    }
+};
