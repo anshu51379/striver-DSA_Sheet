ffe0cfd54cef4120b2aba02b2d7193894c22348c
Abhishek Kumar <80306592+anshu51379@users.noreply.github.com>
2023-06-02 17:29:27 +0530

    Update deleteNodeInBinarySearchTree.cpp

--- Binary Search Trees/deleteNodeInBinarySearchTree.cpp
+++ Binary Search Trees/deleteNodeInBinarySearchTree.cpp
@@ -219,3 +219,39 @@ public:
         return tmp;
     }
 };
+
+//2ndApproach
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(!root){
+            return NULL;
+        }
+        if(root->val==key){
+            if(!root->right){
+                return root->left;
+            }else{
+                TreeNode *curr=root->right;
+                while(curr->left){
+                    curr=curr->left;
+                }
+                swap(root->val, curr->val);
+            }
+        }
+        root->left=deleteNode(root->left, key);
+        root->right=deleteNode(root->right, key);
+        return root;
+    }
+};
