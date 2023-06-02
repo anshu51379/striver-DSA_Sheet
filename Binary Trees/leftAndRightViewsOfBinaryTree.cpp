b06b0618d3291f660e7ad368e5ec46fc375f670f
Abhishek Kumar <80306592+anshu51379@users.noreply.github.com>
2023-06-02 10:49:03 +0530

    Update and rename leftViewOfBinaryTree.cpp to leftAndRightViewsOfBinaryTree.cpp

--- Binary Trees/leftAndRightViewsOfBinaryTree.cpp
+++ Binary Trees/leftAndRightViewsOfBinaryTree.cpp
@@ -148,3 +148,37 @@ vector<int> leftView(Node *root)
    helper(root, 0, res);
    return res;
 }
+
+
+
+// https://leetcode.com/problems/binary-tree-right-side-view/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void helper(vector<int> &res, TreeNode* root, int level){
+        if(!root){
+            return;
+        }
+        if(res.size()==level){
+            res.push_back(root->val);
+        }
+        helper(res, root->right, level+1);
+        helper(res, root->left, level+1);
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        helper(res, root, 0);
+        return res;
+    }
+};
